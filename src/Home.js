325b62778c822efa7248c7ed62d746124b632340
Eddie Kim <jungyupkim4@gmail.com>
2018-09-05 18:15:25 -0400

    Second Commit

--- src/Home.js
+++ src/Home.js
@@ -50,8 +50,6 @@ class Home extends Component {
     constructor(props) {
         super(props);
 
-        super(props);
-
         this.state = {
             espresso: 3,
             water: 3,
@@ -62,8 +60,10 @@ class Home extends Component {
 
     checkRatio = (value) => {
         // console.log(data);
+        let temp;
+
         if (value === 'Americano') {
-            let temp = data[1].ingredient;
+            temp = data[1].ingredient;
 
             this.setState({
                 water: temp[0].ratio,
@@ -71,10 +71,8 @@ class Home extends Component {
                 milk: 0,
                 hotChocolate: 0
             });
-            // console.log(initialPie);
-            // return initialPie;
         } else if (value === 'Latte') {
-            let temp = data[1].ingredient;
+            temp = data[1].ingredient;
 
             this.setState({
                 milk: temp[0].ratio,
@@ -83,31 +81,30 @@ class Home extends Component {
                 hotChocolate: 0
             });
         } else if (value === 'Mocha') {
-            let temp = data[2].ingredient;
-
+            temp = data[3].ingredient;
+            console.log(temp);
             this.setState({
                 milk: 0,
                 espresso: temp[1].ratio,
                 hotChocolate: temp[0].ratio,
                 water: 0
             });
-        } else {
-            document.getElementById('result').innerHTML = "Hello!";
+        } else if (value === 'Favourite') {
+            temp = data[2].ingredient;
+
+            this.setState({
+                milk: temp[0].ratio,
+                espresso: temp[1].ratio,
+                hotChocolate: temp[2].ratio,
+                water: 0
+            });
         }
-        // let temp = data;
-        // console.log(temp[0].ingredient[0].type);
-        //return null;
+        else {
+            document.getElementById('result').innerHTML = "Please check your button name";
+        }
     };
 
     render() {
-
-        //const checkRatio = _.debounce((value) => { this.checkRatio(value) }, 300);
-
-        // if (this.checkRatio() == 'test') {
-        //     console.log('lala');
-        // }
-
-
 
         let milk = ["Milk", this.state.milk];
         let espresso = ["Espresso", this.state.espresso];
@@ -122,13 +119,25 @@ class Home extends Component {
                   <div className="row">
                       <div className="left col-lg-6 mr-auto">
                           <h2 className="text-center subtitle">Select Your Coffee</h2>
-                          <button onClick={()=>this.checkRatio('Americano')}>Americano</button>
-                          <button onClick={()=>this.checkRatio('Latte')}>Latte</button>
-                          <button onClick={()=>this.checkRatio('Mocha')}>Mocha</button>
+                          <button onClick={()=>this.checkRatio('Americano')}>
+                              <img src="http://www.yourcoffeeguru.com/wp-content/uploads/2014/07/a031dc26-f463-4d7e-8d36-6b11ac27a1c7-500x545.jpg" alt="Americano" />
+                              <figcaption>Americano</figcaption>
+                          </button>
+                          <button onClick={()=>this.checkRatio('Latte')}>
+                              <img src="https://www.caffesociety.co.uk/assets/recipe-images/latte-small.jpg" alt="Latte" />
+                              <figcaption>Latte</figcaption>
+                          </button>
+                          <button onClick={()=>this.checkRatio('Mocha')}>
+                              <img src="https://www.panerabread.com/content/dam/panerabread/menu/grid/caffe-mocha-large.jpg" alt="Mocha" />
+                              <figcaption>Mocha</figcaption>
+                          </button>
+                          <button onClick={()=>this.checkRatio('Favourite')}>
+                              <img src="https://www.nespresso.com/ncp/res/resized/w720/uploads/recipes/28578e5a6c40dbb0c9b02f046f670e82b2b9ddd8.jpg" alt="My Favourite" />
+                              <figcaption>My Favourite</figcaption>
+                          </button>
                       </div>
                       <div className="right col-lg-6 mr-auto">
                           <h2 className="text-center subtitle">Your Result</h2>
-                          <p id="result">Yoyoyo</p>
                           <div>
                               <Chart
                                   chartType="PieChart"
